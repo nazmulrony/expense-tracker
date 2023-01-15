9338a97fffdbc7146c9755dca18084a57fcc0dab
Shahadat_Robin <robin@corexlab.dev>
2023-01-15 12:53:31 +0600

    close modal functionality added

---
+++ components/Button.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import { GlobalStyles } from "../constants/style";
+
+const Button = ({ children, onPress, mode, style }) => {
+    return (
+        <View style={style}>
+            <Pressable
+                onPress={onPress}
+                style={({ pressed }) => pressed && styles.pressed}
+            >
+                <View style={[styles.button, mode === "flat" && styles.flat]}>
+                    <Text
+                        style={[
+                            styles.buttonText,
+                            mode === "flat" && styles.flatText,
+                        ]}
+                    >
+                        {children}
+                    </Text>
+                </View>
+            </Pressable>
+        </View>
+    );
+};
+
+export default Button;
+
+const styles = StyleSheet.create({
+    button: {
+        padding: 8,
+        borderRadius: 4,
+        backgroundColor: GlobalStyles.colors.primary500,
+    },
+    flat: {
+        backgroundColor: "transparent",
+    },
+    buttonText: {
+        color: "white",
+        textAlign: "center",
+    },
+    flatText: {
+        color: GlobalStyles.colors.primary200,
+    },
+    pressed: {
+        opacity: 0.75,
+        backgroundColor: GlobalStyles.colors.primary100,
+        borderRadius: 4,
+    },
+});
--- screens/ManageExpenseScreen.js
+++ screens/ManageExpenseScreen.js
@@ -1,21 +1,78 @@
 import { useLayoutEffect } from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import Button from "../components/Button";
+import IconButton from "../components/UI/IconButton";
+import { GlobalStyles } from "../constants/style";
 
 const ManageExpenseScreen = ({ route, navigation }) => {
-    const editedExpenseId = route.params?.expenseId
-    const isEditing = !!editedExpenseId
+    const editedExpenseId = route.params?.expenseId;
+    const isEditing = !!editedExpenseId;
     useLayoutEffect(() => {
         navigation.setOptions({
-            title: isEditing ? 'Edit Expense' : 'Add Expense'
-        })
-    }, [isEditing, navigation])
+            title: isEditing ? "Edit Expense" : "Add Expense",
+        });
+    }, [isEditing, navigation]);
 
+    function deleteExpenseHandler() {
+        navigation.goBack();
+    }
+    function cancelHandler() {
+        navigation.goBack();
+    }
+    function confirmHandler() {
+        navigation.goBack();
+    }
 
     return (
-        <View>
-            <Text>Manage Expense Screen </Text>
+        <View style={styles.container}>
+            <View style={styles.buttonsContainer}>
+                <Button
+                    style={styles.button}
+                    onPress={cancelHandler}
+                    mode="flat"
+                >
+                    Cancel
+                </Button>
+                <Button style={styles.button} onPress={confirmHandler}>
+                    {isEditing ? "Update" : "Add"}
+                </Button>
+            </View>
+            {isEditing && (
+                <View style={styles.deleteContainer}>
+                    <IconButton
+                        size={36}
+                        icon={"trash"}
+                        color={GlobalStyles.colors.error500}
+                        onPress={deleteExpenseHandler}
+                    />
+                </View>
+            )}
         </View>
     );
 };
 
 export default ManageExpenseScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 24,
+        backgroundColor: GlobalStyles.colors.primary800,
+    },
+    buttonsContainer: {
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    button: {
+        minWidth: 120,
+        marginHorizontal: 8,
+    },
+    deleteContainer: {
+        marginTop: 16,
+        paddingTop: 8,
+        borderTopWidth: 2,
+        borderTopColor: GlobalStyles.colors.primary200,
+        alignItems: "center",
+    },
+});
